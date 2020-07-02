a644678fe6c40c01bf65c15b38c9d08fbac344ec
Amritesh Singh <amriteshsingh.oshu@gmail.com>
2020-07-03 02:13:19 +1000

    fixed errors

--- Back-End/api.py
+++ Back-End/api.py
@@ -8,6 +8,18 @@ from datetime import datetime
 app = Flask(__name__)
 api = Api(app, default='MealMatch', title='MealMatch', description='MealMatch')
 
+login_model = api.model('login', {
+    'username': fields.String(required=True, example='heisenberg'),
+    'password': fields.String(required=True, example='heisenberg')
+})
+
+signup_model = api.model('signup', {
+    'username': fields.String(required=True, example='heisenberg'),
+    'password': fields.String(required=True, example='iamthedanger'),
+    'first_name': fields.String(required=True, example='Walter'),
+    'last_name': fields.String(required=True, example='White')
+})
+
 db_name = 'data.db'
 
 
@@ -121,9 +133,13 @@ class login(Resource):
     @api.response(200, 'Login successful')
     @api.response(404, 'Login failed')
     @api.doc(description='User Login')
+    @api.expect(login_model, validate=True)
     def post(self):
-        username = request.args.get('username')
-        password = request.args.get('password')
+        username = api.payload['username']
+        password = api.payload['password']
+
+        # username = request.args.get('username')
+        # password = request.args.get('password')
 
         conn = create_db(db_name)
         c = conn.cursor()
@@ -148,11 +164,17 @@ class Signup(Resource):
     @api.response(201, 'Signup successful')
     @api.response(404, 'Signup failed')
     @api.doc(description='User Signup')
+    @api.expect(signup_model, validate=True)
     def post(self):
-        username = request.args.get('username')
-        password = request.args.get('password')
-        first_name = request.args.get('first_name')
-        last_name = request.args.get('last_name')
+        username = api.payload['username']
+        password = api.payload['password']
+        first_name = api.payload['first_name']
+        last_name = api.payload['last_name']
+
+        # username = request.args.get('username')
+        # password = request.args.get('password')
+        # first_name = request.args.get('first_name')
+        # last_name = request.args.get('last_name')
 
         conn = create_db(db_name)
         c = conn.cursor()
