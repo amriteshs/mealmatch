014c9537b9d1ec87064abca3c938b22f8495e595
Amritesh Singh <amriteshsingh.oshu@gmail.com>
2020-07-03 01:26:42 +1000

    fixed implementation

--- Back-End/api.py
+++ Back-End/api.py
@@ -1,5 +1,5 @@
 import sqlite3
-from flask import Flask
+from flask import Flask, request
 from flask_restplus import Resource, Api, fields, inputs
 import requests
 import json
@@ -7,21 +7,6 @@ from datetime import datetime
 
 app = Flask(__name__)
 api = Api(app, default='MealMatch', title='MealMatch', description='MealMatch')
-
-login_model = api.model('login', {
-    'username': fields.String(required=True, example='heisenberg'),
-    'password': fields.String(required=True, example='heisenberg')
-})
-
-signup_model = api.model('signup', {
-    'username': fields.String(required=True, example='heisenberg'),
-    'password': fields.String(required=True, example='iamthedanger'),
-    'first_name': fields.String(required=True, example='Walter'),
-    'last_name': fields.String(required=True, example='White')
-})
-
-# parser = api.parser()
-# parser.add_argument('q', type=inputs.regex('^(top|bottom)([1-9][0-9]?|100)$'), help='Format: top/bottom<1-100>')
 
 db_name = 'data.db'
 
@@ -31,97 +16,97 @@ def create_db(db_file):
         conn = sqlite3.connect(db_file)
         c = conn.cursor()
 
-        c.execute('CREATE TABLE IF NOT EXISTS User (                                '
-                  '     id              INTEGER         NOT NULL    , '
-                  '     username        VARCHAR(30)     NOT NULL    UNIQUE,         '
-                  '     password        VARCHAR(50)     NOT NULL,                   '
-                  '     first_name      VARCHAR(30)     NOT NULL,                   '
-                  '     last_name       VARCHAR(50)     NOT NULL,                   '
-                  '     PRIMARY KEY(id)                                             '
-                  ')                                                                '
+        c.execute('CREATE TABLE IF NOT EXISTS User (                        '
+                  '     id              INTEGER         NOT NULL,           '
+                  '     username        VARCHAR(30)     NOT NULL    UNIQUE, '
+                  '     password        VARCHAR(50)     NOT NULL,           '
+                  '     first_name      VARCHAR(30)     NOT NULL,           '
+                  '     last_name       VARCHAR(50)     NOT NULL,           '
+                  '     PRIMARY KEY(id)                                     '
+                  ')                                                        '
         )
 
-        c.execute('CREATE TABLE IF NOT EXISTS Ingredient (                          '
-                  '     id              INTEGER         NOT NULL    , '
-                  '     name            VARCHAR(30)     NOT NULL    UNIQUE,         '
-                  '     PRIMARY KEY(id)                                             '
-                  ')                                                                '
+        c.execute('CREATE TABLE IF NOT EXISTS Ingredient (                  '
+                  '     id              INTEGER         NOT NULL,           '
+                  '     name            VARCHAR(30)     NOT NULL    UNIQUE, '
+                  '     PRIMARY KEY(id)                                     '
+                  ')                                                        '
         )
 
-        c.execute('CREATE TABLE IF NOT EXISTS Recipe (                              '
-                  '     id              INTEGER         NOT NULL    , '
-                  '     name            VARCHAR(100)    NOT NULL,                   '
-                  '     description     VARCHAR(500),                               '
-                  '     PRIMARY KEY(id)                                             '
-                  ')                                                                '
+        c.execute('CREATE TABLE IF NOT EXISTS Recipe (                      '
+                  '     id              INTEGER         NOT NULL,           '
+                  '     name            VARCHAR(100)    NOT NULL,           '
+                  '     description     VARCHAR(500),                       '
+                  '     PRIMARY KEY(id)                                     '
+                  ')                                                        '
         )
 
-        c.execute('CREATE TABLE IF NOT EXISTS MealType (                            '
-                  '     id              INTEGER         NOT NULL    , '
-                  '     name            VARCHAR(50)     NOT NULL    UNIQUE,         '
-                  '     PRIMARY KEY(id)                                             '
-                  ')                                                                '
+        c.execute('CREATE TABLE IF NOT EXISTS MealType (                    '
+                  '     id              INTEGER         NOT NULL,           '
+                  '     name            VARCHAR(50)     NOT NULL    UNIQUE, '
+                  '     PRIMARY KEY(id)                                     '
+                  ')                                                        '
         )
 
-        c.execute('CREATE TABLE IF NOT EXISTS Category (                            '
-                  '     id              INTEGER         NOT NULL    , '
-                  '     name            VARCHAR(50)     NOT NULL    UNIQUE,         '
-                  '     PRIMARY KEY(id)                                             '
-                  ')                                                                '
+        c.execute('CREATE TABLE IF NOT EXISTS Category (                    '
+                  '     id              INTEGER         NOT NULL,           '
+                  '     name            VARCHAR(50)     NOT NULL    UNIQUE, '
+                  '     PRIMARY KEY(id)                                     '
+                  ')                                                        '
         )
 
-        c.execute('CREATE TABLE IF NOT EXISTS User_Recipe (                         '
-                  '     id              INTEGER         NOT NULL    , '
-                  '     user_id         INTEGER         NOT NULL,                   '
-                  '     recipe_id       INTEGER         NOT NULL,                   '
-                  '     PRIMARY KEY(id),                                            '
-                  '     FOREIGN KEY(user_id) REFERENCES User(id),                   '
-                  '     FOREIGN KEY(recipe_id) REFERENCES Recipe(id)                '
-                  ')                                                                '
+        c.execute('CREATE TABLE IF NOT EXISTS User_Recipe (                 '
+                  '     id              INTEGER         NOT NULL,           '
+                  '     user_id         INTEGER         NOT NULL,           '
+                  '     recipe_id       INTEGER         NOT NULL,           '
+                  '     PRIMARY KEY(id),                                    '
+                  '     FOREIGN KEY(user_id) REFERENCES User(id),           '
+                  '     FOREIGN KEY(recipe_id) REFERENCES Recipe(id)        '
+                  ')                                                        '
         )
 
-        c.execute('CREATE TABLE IF NOT EXISTS Recipe_Step (                             '
-                  '     id                  INTEGER         NOT NULL    , '
-                  '     recipe_id           INTEGER         NOT NULL,                   '
-                  '     step_no             INTEGER         NOT NULL,                   '
-                  '     step_name           VARCHAR(100),                               '
-                  '     step_description    VARCHAR(1500)   NOT NULL,                   '
-                  '     PRIMARY KEY(id),                                                '
-                  '     FOREIGN KEY(recipe_id) REFERENCES Recipe(id)                    '
-                  ')                                                                    '
+        c.execute('CREATE TABLE IF NOT EXISTS Recipe_Step (                 '
+                  '     id                  INTEGER         NOT NULL,       '
+                  '     recipe_id           INTEGER         NOT NULL,       '
+                  '     step_no             INTEGER         NOT NULL,       '
+                  '     step_name           VARCHAR(100),                   '
+                  '     step_description    VARCHAR(1500)   NOT NULL,       '
+                  '     PRIMARY KEY(id),                                    '
+                  '     FOREIGN KEY(recipe_id) REFERENCES Recipe(id)        '
+                  ')                                                        '
         )
 
-        c.execute('CREATE TABLE IF NOT EXISTS Recipe_Ingredient (                       '
-                  '     id                  INTEGER         NOT NULL    , '
-                  '     recipe_id           INTEGER         NOT NULL,                   '
-                  '     ingredient_id       INTEGER         NOT NULL,                   '
-                  '     ingredient_qty      VARCHAR(25),                                '
-                  '     PRIMARY KEY(id),                                                '
-                  '     FOREIGN KEY(recipe_id) REFERENCES Recipe(id),                   '
-                  '     FOREIGN KEY(ingredient_id) REFERENCES Ingredient(id)            '
-                  ')                                                                    '
+        c.execute('CREATE TABLE IF NOT EXISTS Recipe_Ingredient (           '
+                  '     id                  INTEGER         NOT NULL,       '
+                  '     recipe_id           INTEGER         NOT NULL,       '
+                  '     ingredient_id       INTEGER         NOT NULL,       '
+                  '     ingredient_qty      VARCHAR(25),                    '
+                  '     PRIMARY KEY(id),                                    '
+                  '     FOREIGN KEY(recipe_id) REFERENCES Recipe(id),       '
+                  '     FOREIGN KEY(ingredient_id) REFERENCES Ingredient(id)'
+                  ')                                                        '
         )
 
-        c.execute('CREATE TABLE IF NOT EXISTS Ingredient_Category (                     '
-                  '     id                  INTEGER         NOT NULL    , '
-                  '     category_id         INTEGER         NOT NULL,                   '
-                  '     ingredient_id       INTEGER         NOT NULL,                   '
-                  '     step_name           VARCHAR(100),                               '
-                  '     step_description    VARCHAR(1500)   NOT NULL,                   '
-                  '     PRIMARY KEY(id),                                                '
-                  '     FOREIGN KEY(category_id) REFERENCES Category(id),               '
-                  '     FOREIGN KEY(ingredient_id) REFERENCES Ingredient(id)            '
-                  ')                                                                    '
+        c.execute('CREATE TABLE IF NOT EXISTS Ingredient_Category (         '
+                  '     id                  INTEGER         NOT NULL,       '
+                  '     category_id         INTEGER         NOT NULL,       '
+                  '     ingredient_id       INTEGER         NOT NULL,       '
+                  '     step_name           VARCHAR(100),                   '
+                  '     step_description    VARCHAR(1500)   NOT NULL,       '
+                  '     PRIMARY KEY(id),                                    '
+                  '     FOREIGN KEY(category_id) REFERENCES Category(id),   '
+                  '     FOREIGN KEY(ingredient_id) REFERENCES Ingredient(id)'
+                  ')                                                        '
         )
 
-        c.execute('CREATE TABLE IF NOT EXISTS MealType_Recipe (                         '
-                  '     id                  INTEGER         NOT NULL    , '
-                  '     mealtype_id         INTEGER         NOT NULL,                   '
-                  '     recipe_id           INTEGER         NOT NULL,                   '
-                  '     PRIMARY KEY(id),                                                '
-                  '     FOREIGN KEY(mealtype_id) REFERENCES MealType(id),               '
-                  '     FOREIGN KEY(recipe_id) REFERENCES Recipe(id)                    '
-                  ')                                                                    '
+        c.execute('CREATE TABLE IF NOT EXISTS MealType_Recipe (             '
+                  '     id                  INTEGER         NOT NULL,       '
+                  '     mealtype_id         INTEGER         NOT NULL,       '
+                  '     recipe_id           INTEGER         NOT NULL,       '
+                  '     PRIMARY KEY(id),                                    '
+                  '     FOREIGN KEY(mealtype_id) REFERENCES MealType(id),   '
+                  '     FOREIGN KEY(recipe_id) REFERENCES Recipe(id)        '
+                  ')                                                        '
         )
 
         conn.commit()
@@ -136,10 +121,9 @@ class login(Resource):
     @api.response(200, 'Login successful')
     @api.response(404, 'Login failed')
     @api.doc(description='User Login')
-    @api.expect(login_model, validate=True)
     def post(self):
-        username = api.payload['username']
-        password = api.payload['password']
+        username = request.args.get('username')
+        password = request.args.get('password')
 
         conn = create_db(db_name)
         c = conn.cursor()
@@ -164,12 +148,11 @@ class Signup(Resource):
     @api.response(201, 'Signup successful')
     @api.response(404, 'Signup failed')
     @api.doc(description='User Signup')
-    @api.expect(signup_model, validate=True)
     def post(self):
-        username = api.payload['username']
-        password = api.payload['password']
-        first_name = api.payload['first_name']
-        last_name = api.payload['last_name']
+        username = request.args.get('username')
+        password = request.args.get('password')
+        first_name = request.args.get('first_name')
+        last_name = request.args.get('last_name')
 
         conn = create_db(db_name)
         c = conn.cursor()
@@ -188,7 +171,7 @@ class Signup(Resource):
             'last_name': last_name
         }
 
-        c.execute(f'INSERT INTO User(username, password, first_name, last_name) VALUES(?,?,?,?)', (data['username'], data['password'], data['first_name'], data['last_name']))
+        c.execute(f'INSERT INTO User VALUES(null,?,?,?,?)', (data['username'], data['password'], data['first_name'], data['last_name']))
 
         conn.commit()
         conn.close()
