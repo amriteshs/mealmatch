dc53df4c44ee5eb15bc54f574b6fe8063d1d1959
Rishabh Khurana <rishabhkhurana@192-168-1-113.tpgi.com.au>
2020-07-11 20:25:24 +1000

    recipes search bar added

--- Front-End/tech-nox/src/home.js
+++ Front-End/tech-nox/src/home.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
+import { withTheme } from "@material-ui/core/styles";
 import Drawer from "@material-ui/core/Drawer";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import AppBar from "@material-ui/core/AppBar";
@@ -14,6 +14,12 @@ import InboxIcon from "@material-ui/icons/MoveToInbox";
 import MailIcon from "@material-ui/icons/Mail";
 import Button from '@material-ui/core/Button';
 
+// for search bar
+import SearchIcon from '@material-ui/icons/Search';
+import InputBase from '@material-ui/core/InputBase';
+import { fade, makeStyles } from '@material-ui/core/styles';
+
+
 const drawerWidth = 240;
 
 const useStyles = makeStyles(theme => ({
@@ -22,10 +28,54 @@ const useStyles = makeStyles(theme => ({
   },
   appBar: {
     width: `calc(100% - ${drawerWidth}px)`,
-    marginLeft: drawerWidth
+    marginLeft: drawerWidth,
+    backgroundColor:'black'
+  },
+  searchBar:{
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: drawerWidth,
+    marginTop:'25rem',
+    backgroundColor:'black'
   },
   title: {
     flexGrow: 1,
+  },
+  inputRoot: {
+    color: 'inherit',
+  },
+  search: {
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: fade(theme.palette.common.white, 0.25),
+    },
+    marginRight: theme.spacing(2),
+    marginLeft: 0,
+    width: '20rem',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing(3),
+      width: 'auto',
+    },
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('md')]: {
+      width: '100ch',
+    },
+  },
+  searchIcon: {
+    padding: theme.spacing(0, 2),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   drawer: {
     width: drawerWidth,
@@ -92,6 +142,29 @@ export default function PermanentDrawerLeft() {
           ))}
         </List>
       </Drawer>
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
+
+        {/* This is the search bar */}
+        <AppBar className={classes.searchBar}>
+        <Toolbar>
+        <div className={classes.search}>
+            <div className={classes.searchIcon}>
+              <SearchIcon />
+            </div>
+            <InputBase
+              placeholder="Search for recipes ..."
+              classes={{
+                root: classes.inputRoot,
+                input: classes.inputInput,
+              }}
+              inputProps={{ 'aria-label': 'search' }}
+            />
+          </div>
+        </Toolbar>
+      </AppBar>
+        
+      </main>
     </div>
   );
 }
